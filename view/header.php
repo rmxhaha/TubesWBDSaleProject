d30db4a5ddfc9e6fa16de0af2d93e310f3653adf
rmxhaha <code000123@gmail.com>
2016-10-01 10:55:51 +0700

    basic header

---
+++ view/header.php
@@ -0,0 +1,20 @@
+<div class="header-container">
+	<h1>SaleProject</h1>
+
+	Hi, <?=$this->user["name"]?><br>
+	<a href="#">logout</a>
+
+
+	<table class="table-header-option">
+		<tr>
+			<td>Catalog</td>
+			<td>Your Products</td>
+			<td>Add Product</td>
+			<td>Sales</td>
+			<td>Purchases</td>
+		</tr>
+	</table>
+
+	<h2><?=$this->title?></h2>
+	<hr>
+</div>
