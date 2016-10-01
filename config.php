5ecfd2c69ddd7c33e12f52dab51498af9fe60d89
rmxhaha <code000123@gmail.com>
2016-10-01 10:56:32 +0700

    config file update

--- config.php
+++ config.php
@@ -3,6 +3,6 @@ define("DB_USER","root");
 define("DB_PASS","abcd1234");
 define("DB_HOST","localhost");
 define("DB_NAME","olshop");
-define("TEMPLATE_PATH","html/");
+define("TEMPLATE_PATH","view/");
 
 ?>
