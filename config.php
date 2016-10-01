2b55d1df8b09f0b4037b2e6e1552bd7a8c879758
rmxhaha <code000123@gmail.com>
2016-10-01 21:09:52 +0700

    fix credential

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-define("DB_USER","root");
+define("DB_USER","maximaxl");
 define("DB_PASS","abcd1234");
-define("DB_HOST","localhost");
-define("DB_NAME","olshop");
+define("DB_HOST","localhost:3306");
+define("DB_NAME","marketplace");
 define("TEMPLATE_PATH","view/");
 
 ?>
