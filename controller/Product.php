91468f4069099879c8eb40bf572e35f4930bc7f1
rmxhaha <code000123@gmail.com>
2016-10-02 09:56:25 +0700

    basic ProductController structure

---
+++ controller/Product.php
@@ -0,0 +1,33 @@
+<?php
+require "controller/Base.php";
+
+class ProductController extends Base{
+	function __construct(){
+		Base::__construct();
+	}
+	
+	function your_products_page(){
+		
+	}
+	
+	function get_product_by_user_id(){
+		
+	}
+	
+	function add_product_form(){
+		
+	}
+	
+	function add_product(){
+		
+	}
+	
+	function edit_product_form(){
+		
+	}
+	
+	function edit_product(){
+		
+	}
+}
+?>
