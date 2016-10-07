e56245b125a0a61e08e7d87a3dca955476172d36
rmxhaha <code000123@gmail.com>
2016-10-07 22:53:15 +0700

    fix catalog product styling

--- view/catalog_product.php
+++ view/catalog_product.php
@@ -4,18 +4,22 @@
   <hr>
   <table>
   <tr>
-    <td><img width=200 src="<?=$this->product_image?>" /></td>
-    <td>
-      <h4><b><?=$this->product_name?></b></h4>
+    <td class="catalog-product-left"><img width=100 src="<?=$this->product_image?>" /></td>
+    <td class="catalog-product-mid">
+      <b><?=$this->product_name?></b><br>
       <?=$this->product_price?><br>
       <?=$this->product_description?>
     </td>
-    <td>
-      <div><span class="like_count_<?=$this->product_id?>"><?=$this->like_count?></span> Likes </div>
-      <div><span class="purchase_count_<?=$this->product_id?>"><?=$this->purchase_count?></span> Purchases </div>
+    <td class="catalog-product-right">
+      <div class="small-description">
+        <div><span class="like_count_<?=$this->product_id?>"><?=$this->like_count?></span> Likes </div>
+        <div><span class="purchase_count_<?=$this->product_id?>"><?=$this->purchase_count?></span> Purchases </div>
+      </div>
+      <br>
       <a class="blue" href="javascript:void(0)" onclick="like(this,<?=$this->product_id?>)"><?=$this->like_button_text?></a>
       <a class="green" href="javascript:void(0)" onclick="buy(this,<?=$this->product_id?>)">BUY</a>
     </td>
   </tr>
   </table>
+  <hr>
 </div>
