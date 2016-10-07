bd27cae855d88c9eb0edb3bd54b1efe893b308cf
rmxhaha <code000123@gmail.com>
2016-10-07 17:58:07 +0700

    javascript validation for purchase

--- public/javascripts/shop.js
+++ public/javascripts/shop.js
@@ -23,9 +23,44 @@ function update_total_price(element){
 }
 
 function money_f(x){
+  if( x < 0 ) x = 0;
   var r = Math.floor( x / 1000 );
   if( r == 0 )
     return "IDR " + x;
   else
     return money_f( r ) + "," + x % 1000;
+}
+
+function validate_purchase(){
+  var q = document.getElementsByName("quantity")[0].value;
+  var errors = [];
+  if( isNaN(parseInt(q) ) )
+    errors.push("Quantity must be a number");
+  else if( parseInt(q) < 1 )
+    errors.push("Quantity must be at least 1");
+
+  var ccn = document.getElementsByName("credit_card_number")[0].value;
+  var digit12 = /^\d{12}$/;
+  if( !ccn.match(digit12) ){
+    errors.push("Credit Card must be 12 digits");
+  }
+
+  var digit3 = /^\d{3}$/;
+  var ccv = document.getElementsByName("credit_card_verification")[0].value;
+  if( !ccv.match(digit3) ){
+    errors.push("Credit Card Verification Number must be 3 digits");
+  }
+
+  var errors_container = document.getElementsByClassName("error-container")[0];
+  errors_container.innerHTML = "";
+
+  for( var i in errors ){
+    var d = document.createElement("div");
+    d.setAttribute("class","error");
+    d.innerHTML = errors[i];
+    errors_container.appendChild(d);
+  }
+
+
+  return ( errors.length == 0 );
 }
