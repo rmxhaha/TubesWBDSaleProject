59fbe5d0cb052f38b297cb4ac44d3d2b328fc90d
rmxhaha <code000123@gmail.com>
2016-10-08 14:01:26 +0700

    User::register return User object from db

--- model/User.php
+++ model/User.php
@@ -26,7 +26,7 @@ class User extends Model {
       VALUES ('$data[fullname]','$data[username]','$data[email]','$data[password]','$data[fulladdress]','$data[postcode]','$data[phone]')
     ";
     if( $result = $db->query($query) ){
-      return User::with_row($data);
+      return User::with_id($db->insert_id);
     }
     return false;
   }
